6b4c303922a4a1927f00b7b8996e8c960481b039
ramonliu <raymond.liu@tron.network>
2025-04-03 14:35:00 +0800

    init

---
+++ src/main/java/ethereum/ckzg4844/Main.java
@@ -0,0 +1,22 @@
+package ethereum.ckzg4844;
+
+import java.io.File;
+
+public class Main {
+  public static void main(String[] args) {
+    try {
+      System.out.println("开始加载jni");
+      CKZG4844JNI.loadNativeLibrary();
+
+      // once a trusted setup is loaded, all methods will use it
+      System.out.println("开始加载文件");
+      String path = "trusted_setup.txt";
+      CKZG4844JNI.loadTrustedSetup(path, 0);
+      System.out.println("加载文件成功");
+    } catch (Exception e) {
+      System.out.println("正常报错!");
+      throw e;
+    }
+    System.out.println("正常结束");
+  }
+}
